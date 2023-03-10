860f3a3cab4666c27f88842fffb35d7772cb1664
Dev Shah <busycaesar@gmail.com>
2023-03-10 17:21:07 -0500

    Make sure that everything is loaded before printing as a pdf.

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
 
   // This navigates to the URL.
-  await page.goto("Web_Site_URL");
+  await page.goto("Website URL");
 
   // This make sures that the login is loaded before moving further.
   await page.waitForSelector("#PayRollNum");
 
   // This enters the login credentials.
-  await page.type("#PayRollNum", "CustomerID");
-  await page.type("#UserName", "UserName");
+  await page.type("#PayRollNum", "Employee Id");
+  await page.type("#UserName", "Username");
   await page.type("#Password", "Password");
 
   // This clicks the login button.
@@ -28,11 +28,17 @@ const puppeteer = require("puppeteer");
   // This clicks the pay stubs button.
   await page.click("#NetPay");
 
-  // This make sures that the page is loaded before moving further.
-  await page.waitForSelector(".netPayContainer");
+  // This make sures that all the elements are loaded before moving further.
+  await page.waitForSelector("#Earnings1");
+  await page.waitForSelector("#Deductions1");
+  await page.waitForSelector("#Additional1");
 
   // This downloads the pdf copy of the pay slip.
-  await page.pdf({ path: "payslip[Month_Year].pdf", format: "A4", pageRanges: "1-1" });
+  await page.pdf({
+    path: "Payslip_Month_Year.pdf",
+    format: "A4",
+    pageRanges: "1-1",
+  });
 
   // This finally closes the browser.
   await browser.close();
